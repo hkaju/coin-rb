ef0092feab39638098ec333df1d3d480a29225a4
Hendrik Kaju <hendrik.kaju@gmail.com>
2013-06-25 22:22:45 +0300

    Initial version

--- coin.rb
+++ coin.rb
@@ -1,3 +1,19 @@
 #!/usr/bin/env ruby
+require 'rubygems'
+require 'json'
+require 'pp'
 
+json = File.read("movies.txt")
+movies = JSON.parse(json)
 
+pool = []
+pp movies
+movies.each do |id,movie|
+  weight = movie["rating"] * 10
+  weight.to_i.times do pool << id end
+end
+
+puts pool.count
+
+gold = pool.choice
+exec("open http://www.imdb.com/title/" << gold)
