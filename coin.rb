469afc9d05dd8409437f1436435b2d8f539857c8
Hendrik Kaju <hendrik.kaju@gmail.com>
2013-06-27 21:20:35 +0300

    Coding style changes

--- coin.rb
+++ coin.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'json'
-require 'pp'
-require 'themoviedb'
+require "rubygems"
+require "json"
+require "pp"
+require "themoviedb"
 
 Tmdb::Api.key("11433deecaf09ef3aa3fb68d7e02a772")
 
-DBFILE = '.coindb.json'
-DB_LOCATION = '~/'
+DBFILE = ".coindb.json"
+DB_LOCATION = "~/"
 
 class ::Hash
   def method_missing(name)
@@ -18,81 +18,89 @@ class ::Hash
   end
 end
 
-def read_database()
-  db_path = DB_LOCATION + DBFILE
-  if File.exists? File.expand_path(db_path):
-    json = File.read(File.expand_path(db_path))
-    @movies = JSON.parse(json)
-  else
-    @movies = Hash.new
+
+class MoviePool
+  
+  def initialize
+    self.read_database
   end
-end
-
-def write_database
-  File.open(File.expand_path(DB_LOCATION + DBFILE), 'w') { |f| f.write(@movies.to_json) }
-end
-
-def flip
-  read_database
-  pool = []
-
-  @movies.each do |tmdb, movie|
-    weight = movie.rating * 10
-    weight.to_i.times do pool << tmdb end
+  
+  def read_database()
+    db_path = DB_LOCATION + DBFILE
+    if File.exists? File.expand_path(db_path):
+      json = File.read(File.expand_path(db_path))
+      @movies = JSON.parse(json)
+    end
   end
 
-  random_movie = @movies[pool.choice]
-  return random_movie
-end
-
-def add(title, uri=nil)
-  read_database
-  movie = Tmdb::Movie.find(title)[0]
-  @movies[movie.id.to_s] = {'title' => movie.title,
-                       'rating' => movie.vote_average,
-                       'tmdb' => movie.id,
-                       'released' => movie.release_date}
-  @movies[movie.id.to_s]['uri'] = uri if uri
-  write_database
-  puts "Added #{ movie.title } (#{ movie.release_date[0..3] })"
-end
-
-def list
-  read_database
-  puts "TMDB ID - Title\n\n"
-  @movies.each do |key, movie|
-    puts "#{ key } - #{ movie.title } (#{ movie.released[0..3] })"
+  def write_database
+    File.open(File.expand_path(DB_LOCATION + DBFILE), "w") { |f| f.write(@movies.to_json) }
   end
-end
 
-def remove(tmdb)
-  read_database
-  @movies.delete(tmdb)
-  write_database
+  def flip
+    self.read_database
+    pool = []
+
+    @movies.each do |tmdb, movie|
+      weight = movie.rating * 10
+      weight.to_i.times do pool << tmdb end
+    end
+
+    random_movie = @movies[pool.choice]
+    return random_movie
+  end
+
+  def add(title, uri=nil)
+    self.read_database
+    movie = Tmdb::Movie.find(title)[0]
+    @movies[movie.id.to_s] = {"title" => movie.title,
+                              "rating" => movie.vote_average,
+                              "tmdb" => movie.id,
+                              "released" => movie.release_date}
+    @movies[movie.id.to_s]["uri"] = uri if uri
+    self.write_database
+    puts "Added #{ movie.title } (#{ movie.release_date[0..3] })"
+  end
+
+  def list
+    self.read_database
+    puts "TMDB ID - Title\n\n"
+    @movies.each do |key, movie|
+      puts "#{ key } - #{ movie.title } (#{ movie.released[0..3] })"
+    end
+  end
+
+  def remove(tmdb)
+    self.read_database
+    @movies.delete(tmdb)
+    self.write_database
+  end
+
 end
 
 if __FILE__ == $0
   action = ARGV[0]
+  pool = MoviePool.new
   case action
-  when 'flip', 'f'
-    movie = flip
+  when "flip", "f"
+    movie = pool.flip
     puts movie.title if movie
-  when 'watch', 'w'
+  when "watch", "w"
     if ARGV[1]
-      watch ARGV[1]
+      pool.watch ARGV[1]
     else
-      watch
+      pool.watch
     end
-  when 'add', 'a'
+  when "add", "a"
     if ARGV[2]
       # TODO URI validation
-      add(ARGV[1], ARGV[2])
+      pool.add(ARGV[1], ARGV[2])
     else
-      add ARGV[1]
+      pool.add ARGV[1]
     end
-  when 'list', 'l'
-    list
-  when 'rm', 'del', 'delete', 'remove', 'd'
-    remove ARGV[1]
+  when "list", "l"
+    pool.list
+  when "rm", "del", "delete", "remove", "d"
+    pool.remove ARGV[1]
   end
 end
